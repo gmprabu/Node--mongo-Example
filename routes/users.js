8935d10671be3ad6d3e6ca814b8c7557bc6ef1fa
gmprabu <mit.prabu8@gmail.com>
2017-12-25 19:22:24 +0530

    implemented curd users

--- routes/users.js
+++ routes/users.js
@@ -6,53 +6,34 @@ var User = require('../model/user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  // get all the users
+  User.find({}, function(err, users) {
+    if (err) throw err;
+   // res.send(users);
+    res.render('user', { userList: users });
+    // object of all the users
+    console.log(users);});
+});
+
+router.get('/create', function(req, res, next) {
+  res.render('create');
+});
+
+router.post('/',function(req,res,next){
   
-
-
-
-// create a new user called chris
-var chris = new User({
-  name: 'Chris',
-  username: 'starlord55',
-  password: 'password' ,
-  admin : true
+  let user = new User(req.body);
+  user.save(req.body,function(err) {
+    if (err) throw err;
+    res.send(user);
+  });
 });
 
-// call the custom method. this will just add -dude to his name
-// user will now be Chris-dude
-chris.dudify(function(err, name) {
-  if (err) throw err;
-
-  console.log('Your new name is ' + name);
+router.put('/:id', function(req, res, next) {
+  User.findByIdAndUpdate(req.params.id, req.body,{new: true}, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
 });
 
-// call the built-in save method to save to the database
-chris.save(function(err) {
-  if (err) throw err;
-
-  res.send('User saved successfully!');
-  console.log('User saved successfully!');
-});
-
-// get all the users
-User.find({}, function(err, users) {
-  if (err) throw err;
-
-  // object of all the users
-  console.log(users);
-});
-
-// get the user starlord55
-User.find({ username: 'starlord55' }, function(err, user) {
-  if (err) throw err;
-
-  // object of the user
-  console.log(user);
-});
-
-
-
-
-});
 
 module.exports = router;
